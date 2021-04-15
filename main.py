02ea28baaf0abd03d9c912d773f984b0a82536c8
mindolink <ice.cvicek@gmail.com>
2021-04-15 23:46:07 +0200

    lep

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ PathUserSchedule='./ImportData/userSchedule.xlsx'
 PathAbiSC='./SmartConcract/abiSystemControlingConcract.json'
 PathAbiEB='./SmartConcract/abiElectricityBillingConcract.json'
 dt=30
-t=1.2
+t=1.5
 
 Day=1   
 Hour=0
